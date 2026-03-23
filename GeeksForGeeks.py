2e68c0dae2df84ead6def85cb1aec15a1913ad99
rima-sk135 <rima.sk135@gmail.com>
2026-03-23 23:35:15 +0530

    Add files via upload

---
+++ GeeksForGeeks.py
@@ -0,0 +1,37 @@
+print("Hello World, I don't give a bug!")
+#sample comment
+#This is Python comment
+""" this is a
+multiline
+ comment"""
+name = "Geeks for Geeks"
+print(name)
+
+#DATA TYPES in Python - Numeric (Integer, Float, Complex no.); Dictionary; Boolean; Set; Sequence type (String, List, Tuple)..
+
+x = "Hello World" # string
+x = 50 # integer
+x = 60.5 # float
+x = 6j # complex number
+
+
+x = ["geeks", "for", "geeks"] # list
+x = {"geeks", "for", "geeks"} # set
+x = {"name": "Rima", "age": "21"} # dict
+x = ("geeks", "for", "geeks") # tuple
+x = True # bool
+x = b"Geeks" # binary
+print(type(x))
+
+# Input and output python prog
+val = input("enter your value")
+print("you entered: ", val)
+
+#The input() function in Python always returns data as a string, regardless of what the user enters. If we want to store the input as another data type (like int, float, etc.), we need to explicitly convert (typecast) it.
+name = input("enter your name: ")
+print(type(name))
+age = input("enter your age: ")
+print(type(age))
+#Now typecasting the age variable as an integer
+age = int(input("Enter your age: "))
+print(type(age))
